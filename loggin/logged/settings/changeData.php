43a9ecd6882099e3208c748d19e02c97fa45ed09
Ksymena P <ksymenka@duck.com>
2024-03-05 12:25:33 +0100

    added not working info update

--- loggin/logged/settings/changeData.php
+++ loggin/logged/settings/changeData.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <header>
-        <a href="./settings.php">Ustawienia użytkownika <?php echo $username ?></a> 
+        <a href="../settings.php">Ustawienia użytkownika <?php echo $username ?></a> 
         <a href="news.html">Wiadomości</a>
     </header>
 
@@ -18,12 +18,61 @@
         <p><input id="email" type="email" name="email" autocomplete="username" placeholder="Adres e-mail"></p>
         <p><input type="number" style="width: 95%;" name="nr_tel" id="nr_tel" maxlength="9" placeholder="Numer telefonu" min="0"></p>
         <p><input id="password" autocomplete="new-password" type="password" name="haslo" placeholder="Hasło"></p>
-        <p><input id="repeatPassword" autocomplete="new-password" type="password" placeholder="Powtórz hasło"></p>
-        <p><input id="submit" type="submit" value="Zarejestruj się"></p>
-        <p id="err"></p>               
+        <p><input id="submit" type="submit" value="Zaktualizuj dane"></p>
+        <p id="err">               
+        <?php
+        $servername = "127.0.0.1"; 
+        $username = "Admin";
+        $password = "!QAZ2wsx";
+        $db_name = "serwis_komputerowy";
+        
+        $conn = new mysqli($servername, $username, $password, $db_name);
+        $username = $_COOKIE['Username'];
+        $id = $conn -> query("SELECT id FROM uzytkownik WHERE email='$username'");
+        
+        $options = [
+            'cost' => 12,
+        ];
+        function checkIfExist($conn, $email) {
+            $dataBasemail = null;
+            $stmt = $conn->prepare("SELECT email FROM uzytkownik WHERE email= ?");
+            $stmt -> bind_param("s", $email);
+            $stmt -> execute();
+            $stmt -> bind_result($dataBasemail);
+            $stmt -> fetch();
+            $stmt -> close();
+            return $dataBasemail == $email;
+        }
+        $isEmpty = false;
+        foreach ($_POST as $field) {
+            if (empty($field)) {
+                echo "Pole", $field, " nie może być puste !<br>";
+                $isEmpty = true;
+            }
+        }
+        if (!$isEmpty) {
+            $imie = $conn -> real_escape_string($_POST['imie']);    
+            $nazwisko = $conn -> real_escape_string($_POST['nazwisko']);    
+            $email = $conn -> real_escape_string($_POST['email']);    
+            $nr_tel = $conn -> real_escape_string($_POST['nr_tel']);    
+            $haslo = password_hash($conn -> real_escape_string($_POST['haslo']), PASSWORD_DEFAULT, $options);    
+            
+            if(checkIfExist($conn, $email)) {
+                $sql = "UPDATE `uzytkownik` SET imie='$imie', nazwisko='$nazwisko', email='$email', nr_tel='$nr_tel', haslo='$haslo' WHERE id='$id'";
+
+                if ($conn -> query($sql) === TRUE) {
+                    echo "Zaktualizowano dane";
+                    $conn -> close();
+                    header("Location: ../user.php");
+                    exit();
+                } else {
+                    echo "Błąd: " . $sql . "<br>" . $conn->error;
+                }
+            }
+            }
+         ?>
+        </p>
+
     </form>
-    
-    <script src="../registration.js"></script>
-    
 </body>
 </html>
